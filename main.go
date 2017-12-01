1255c6a685b9ccebee6e1cf8719a86a58ed022bc
Ashwini Shankar Patankar <ashwinispatankar@gmail.com>
2017-12-01 10:56:22 -0400

    Added Gonfig example

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"os"
+
+	"github.com/tkanos/gonfig"
 )
 
 type Configuration struct {
@@ -13,6 +16,17 @@ type Configuration struct {
 }
 
 func main() {
+	fmt.Println("*************************************************")
+	fmt.Println("\t\t\tJSON Example\t\t\t")
+	fmt.Println("*************************************************")
+	jsonExample()
+	fmt.Println("*************************************************")
+	fmt.Println("\t\t\tGOING Example\t\t\t")
+	fmt.Println("*************************************************")
+	goingExample()
+	fmt.Println("*************************************************")
+}
+func jsonExample() {
 	//filename is the path to the json config file
 	filename := "config/config.development.json"
 	file, err := os.Open(filename)
@@ -27,6 +41,18 @@ func main() {
 		log.Println(err)
 	}
 	configuration.Connection_String = os.Getenv("Connection_String")
+
+	log.Println(configuration.Port)
+	log.Println(configuration.Connection_String)
+	log.Println(configuration.Static_Variable)
+}
+
+func goingExample() {
+	configuration := Configuration{}
+	err := gonfig.GetConf("config/config.production.json", &configuration)
+	if err != nil {
+		panic(err)
+	}
 
 	log.Println(configuration.Port)
 	log.Println(configuration.Connection_String)
